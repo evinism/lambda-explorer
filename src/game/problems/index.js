7cd8ad4ade210ceb0161deb767b5ce92c0f342f6
Evin Sellin <evinism@gmail.com>
2017-05-10 08:19:35 -0700

    remove unnecessary number field from problems

--- src/game/problems/index.js
+++ src/game/problems/index.js
@@ -2,7 +2,6 @@
 // interface for each should be roughly:
 /*
   {
-    number: [num],
     title: 'string',
     prompt: 'string',
     winCondition: computationData => bool
@@ -10,13 +9,6 @@
 */
 export default [
   {
-    number: 1,
-    title: 'Are you dead?',
-    prompt: 'Type something in. This is the hardest problem out of all of them',
-    winCondition: ({text}) => text !== '',
-  },
-  {
-    number: 2,
     title: 'Simple Identifier',
     prompt: "First, type 'a₁'. Letters followed optionally by numbers represent variables in the lambda calculus.",
     winCondition: ({text}) => {
@@ -25,7 +17,6 @@ export default [
   },
   // okay the first problem I actually care about
   {
-    number: 3,
     title: 'Identity',
     prompt: "Nice! Next, we'll get you started with some basic syntax. We're gonna make the identity function. please type λa₁.a₁ or something",
     winCondition: ({ast}) => {
@@ -39,7 +30,6 @@ export default [
     }
   },
   {
-    number: 4,
     title: "Parentheses",
     prompt: "Schweet! This takes one argument a and outputs that same argument! Now go ahead and wrap the whole thing in parentheses",
     winCondition: ({text, ast}) => {
@@ -54,7 +44,6 @@ export default [
     }
   },
   {
-    number: 5,
     title: "Baby's first β-reduction",
     prompt: "Nicely done! You can always wrap an expression inside parentheses. With that out of the way, we can apply variables to this function! note: not passable because not implemented yet...",
     winCondition: ({ast}) => {
