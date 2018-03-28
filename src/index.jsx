50aa121b1f5cc3a9cbabea8c8282142004321e33
Evin Sellin <evinism@gmail.com>
2018-03-28 13:37:25 -0700

    remove jquery dep

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import $ from 'jquery';
 
 import App from './components/App';
 
-$(document).ready(() => {
-  ReactDOM.render(<App />, $('#react-mount')[0]);
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(<App />, document.getElementById('react-mount'));
 });
